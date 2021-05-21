7d3113501f6173e48c282339f857ff44cf3ba710
pangerkarpranay2896 <pangerkarpranay.2896@gmail.com>
2021-05-21 23:57:03 +0530

    Java project added for Springboot

---
+++ webtuts/src/main/java/com/pranaypangerkar/sbet/webtuts/WebtutsApplication.java
@@ -0,0 +1,13 @@
+package com.pranaypangerkar.sbet.webtuts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtutsApplication.class, args);
+	}
+
+}
---
+++ webtuts/src/test/java/com/pranaypangerkar/sbet/webtuts/WebtutsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranaypangerkar.sbet.webtuts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtutsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
